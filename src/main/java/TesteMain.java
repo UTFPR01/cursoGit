6494480d01d858c5599544d9d77c5323ec92e5ce
Claudio Reis <claudio_f_reis@outlook.com>
2024-12-16 15:22:17 -0300

    Adicionando mensagem de exclusao

--- src/main/java/TesteMain.java
+++ src/main/java/TesteMain.java
@@ -1,4 +1,10 @@
 package PACKAGE_NAME;
 
 public class TesteMain {
+    public static void main(String[] args) {
+        System.out.println("Depois da ultima exclusao");
+
+        int j = 0;
+        j++;
+    }
 }
