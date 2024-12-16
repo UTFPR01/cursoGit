ab0eea5411829eef30501e4d9942d700b2f4064d
Claudio Reis <claudio_f_reis@outlook.com>
2024-12-16 15:32:08 -0300

    criando classe Pessoa e instanciand

---
+++ src/main/java/Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa {
+private int cpf;
+private String nome;
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+}
--- src/main/java/TesteMain.java
+++ src/main/java/TesteMain.java
@@ -1,11 +1,11 @@
-package PACKAGE_NAME;
+
 
 public class TesteMain {
     public static void main(String[] args) {
+        Pessoa p = new Pessoa();
         System.out.println("Depois da ultima exclusao");
 
         int j = 0;
         j++;
-        System.out.println(j);
     }
 }
