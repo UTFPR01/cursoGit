d781e5f78d1b07a2905b4f2c1fa4a630698727fb
Claudio Reis <claudio_f_reis@outlook.com>
2024-12-18 15:51:24 -0300

    commit da classe teste no svg

--- src/main/java/TesteMain.java
+++ src/main/java/TesteMain.java
@@ -4,6 +4,7 @@ public class TesteMain {
     public static void main(String[] args) {
         Pessoa p = new Pessoa();
         System.out.println("Depois da ultima exclusao");
+        System.out.println("Novo branch");
 
         int j = 0;//adicionei
         j++;
