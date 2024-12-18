43ff244db5f4c6088e89e43a661cb68b03e554f1
Claudio Reis <claudio_f_reis@outlook.com>
2024-12-18 15:34:02 -0300

    adicionado codigo funcionario

--- src/main/java/Agencia.java
+++ src/main/java/Agencia.java
@@ -1,7 +1,8 @@
 public class Agencia {
 
-    private int conta; //adicionei
+    private int conta; //adicinado novamente
     private String titular;
+    private int codigoFuncionario = 0;
 
     public int getConta() {
         return conta;
